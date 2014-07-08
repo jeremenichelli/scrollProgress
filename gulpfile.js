91d563ddab507c800c912231ed38a4950c68eb18
Jere Menichelli <jmenichelli@gmail.com>
2014-07-07 23:45:32 -0300

    Lint. Minified version added. New styles on showroom page.

---
+++ gulpfile.js
@@ -0,0 +1,33 @@
+var gulp = require('gulp'),
+    uglify = require('gulp-uglify'),
+    jshint = require('gulp-jshint'),
+    rename = require('gulp-rename'),
+    concat = require('gulp-concat-util'),
+    package = require('./package.json');
+
+var paths = {
+        output : 'dist/',
+        scripts : 'src/' + package.name + '.js',
+        test: 'test/spec/' + package.name + '.spec.js'
+    },
+    banner = {
+        top: '// ' + package.title + ' - ' + package.author + '\n' +
+            '// ' + package.repository.url + ' - MIT License\n'
+    };
+
+gulp.task('lint', function(){
+    return gulp.src(paths.scripts)
+        .pipe(jshint())
+        .pipe(jshint.reporter('jshint-stylish'));
+});
+
+gulp.task('minify', ['lint'], function(){
+    return gulp.src(paths.scripts)
+        .pipe(concat.header(banner.top))
+        .pipe(gulp.dest(paths.output))
+        .pipe(uglify())
+        .pipe(rename({ suffix: '.min' }))
+        .pipe(gulp.dest(paths.output));
+});
+
+gulp.task('default', ['minify']);
