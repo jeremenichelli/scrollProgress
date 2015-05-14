35b388599e5abda363e5eccbd910417ba3b495cc
Jere Menichelli <jmenichelli@gmail.com>
2015-05-14 00:15:10 -0300

    fix test names

--- test/scrollProgress.spec.js
+++ test/scrollProgress.spec.js
@@ -8,10 +8,10 @@
         it('set method available in global scope', function() {
             expect(typeof root.scrollProgress.set).toBe('function');
         });
-        it('namespace available in global scope', function() {
+        it('trigger method available in global scope', function() {
             expect(typeof root.scrollProgress.trigger).toBe('function');
         });
-        it('namespace available in global scope', function() {
+        it('update method available in global scope', function() {
             expect(typeof root.scrollProgress.update).toBe('function');
         });
     });
